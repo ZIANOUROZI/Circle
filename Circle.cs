c2155b67f1f7e5f2f40c1a3eb5ec486789f86817
Ziano <Ziano@ZIA>
2023-10-15 23:03:49 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    internal class Circle
+    {
+       private int _Radius; // This is a private field for radius
+        
+        public Circle(int radius) // This is a constructor for the Circle 
+        {
+            _Radius = radius; // This is constructor for the value of the radius
+        }       
+        public double GetArea()  => Math.Pow(_Radius, 2) * Math.PI; // This is the publice method for GetArea and Get the results
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyCircle = new Circle(5); //This line creates an instance of the Circle Class 
+            Console.WriteLine(MyCircle.GetArea()); // This line calls the GetArea method and MyCircle objekt
+            Console.WriteLine(Math.Round(MyCircle.GetArea(), 2)); // This line calls again GetArea and MyCircle but i us Math.Round to round the area to 2 decimal
+
+            Circle MyCircel2 = new Circle(6); //This line creates an instance of the Circle Class 
+            Console.WriteLine(MyCircel2.GetArea());  // This line calls the GetArea method and MyCircle objekt
+            Console.WriteLine(Math.Round(MyCircel2.GetArea(), 2)); // This line calls again GetArea and MyCircle but i us Math.Round to round the area to 2 decimal
+        }
+    }
+}
